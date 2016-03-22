d0a1487fc87597915178bbad1c91f59c3647138c
vdmkvrshn <vadim.kovyrshin@gmail.com>
2016-03-22 14:06:06 +0200

    Getters has been added.

--- src/main/java/com/parksystem/model/ParkingSpot.java
+++ src/main/java/com/parksystem/model/ParkingSpot.java
@@ -12,6 +12,26 @@ public class ParkingSpot {
 	private int spotNumber;
 	private Level level;
 	
+	public Vehicle getVehicle() {
+		return vehicle;
+	}
+
+	public VehicleSize getSpotSize() {
+		return spotSize;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public int getSpotNumber() {
+		return spotNumber;
+	}
+
+	public Level getLevel() {
+		return level;
+	}
+
 	public ParkingSpot(Level lvl, int r, int n, VehicleSize sz) {
 		level = lvl;
 		row = r;
